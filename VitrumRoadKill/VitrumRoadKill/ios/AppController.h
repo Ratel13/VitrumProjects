ee515b625f63544a237b509ebd2a513c45425f99
Carlos Pinan <carlos.pinan@gmail.com>
2014-04-03 16:40:50 -0500

    VitrumRoadKill

---
+++ VitrumRoadKill/VitrumRoadKill/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  VitrumRoadKillAppController.h
+//  VitrumRoadKill
+//
+//  Created by NSS on 4/3/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ VitrumRoadKill/VitrumRoadKill/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  VitrumRoadKillAppController.h
+//  VitrumRoadKill
+//
+//  Created by NSS on 4/3/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
